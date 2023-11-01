ff2f3fe9a6b80bb551a0af09e6912b2aa0588367
SamanodyJr <sarahelsamanody@aucegypt.edu>
2023-11-01 03:07:15 +0200

    function 4 & 5 finalized

---
+++ function_4_5.h
@@ -0,0 +1,104 @@
+#ifndef FUNCTION_4_5
+#define FUNCTION_4_5
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+void print(vector<string> EPIs, vector<int> notcoveredmins, string variables)
+{
+    //printing EPIS
+    cout << "\n\n\n~~~~~~~~~~~~~~~EPIS~~~~~~~~~~~~~~~\n\n";
+    for(int i = 0 ; i < EPIs.size() ; i++)
+    {   
+        for(int j = 0 ; j < EPIs[i].length(); j++)
+        {
+            if(EPIs[i][j] == '1')
+                cout << variables[j] ;
+            else if(EPIs[i][j] == '0')
+                cout << variables[j] << '\'' ;
+            
+        }
+    if(i != EPIs.size() -1)
+        cout << '+' ;
+    }
+    cout << endl;
+    cout << "\n\n\n~~~~~~~~~MINTERMS NOT COVERED BY EPIS~~~~~~~~~\n\n";
+    if(notcoveredmins.size() == 0)
+        cout << "\tAll minterms are covered by EPIS \n\n";
+    else
+    for(int i = 0 ; i < notcoveredmins.size() ; i++)
+    {
+        cout << "m" << notcoveredmins[i] << endl;
+    }
+}
+
+void function_4_5(map<char , vector<int> > table, vector<string> PIs , vector< vector<int> > minterms , set<char> vars )
+{   
+    string variables;
+    int i (0);
+
+    for(auto it = vars.begin(); it!= vars.end(); it++)
+    {
+        variables[i++] = *it;
+    }
+    
+    int n = table['X'].size();
+
+    map <int , int> mintermfreq;
+    map < int , vector<string> > mintermPIs;
+    vector<string> EPIs;
+    vector<int> notcoveredmins;
+
+
+    for(int i = 0; i < n; i++)
+    if(table['X'][i])
+    {
+        mintermfreq[i] = 0;
+    }
+    for( int i = 0 ; i < minterms.size(); i++)
+        for( int j = 0; j < minterms[i].size(); j++)
+        {
+            mintermfreq[minterms[i][j]]++; 
+            mintermPIs[minterms[i][j]].push_back(PIs[i]);
+        }
+    
+    for(auto it = mintermfreq.rbegin() ;it != mintermfreq.rend(); ++it)
+    {
+        if(it->second == 1)
+        {
+            auto ier = find(EPIs.begin(),EPIs.end(),mintermPIs[it->first][0]);
+            if(ier == EPIs.end())
+                EPIs.push_back(mintermPIs[it->first][0]);
+        }
+    }
+    for(auto it = mintermfreq.begin() ;it != mintermfreq.end(); ++it)
+    {
+        int count(0);
+        if(it->second != 1)
+        {
+            for(int i = 0 ; i < mintermPIs[it->first].size() ; i++)
+            {   for(int j = 0; j < EPIs.size(); j++)
+                {   
+                    if(mintermPIs[it->first][i] == EPIs[j]) 
+                        count++;
+                }
+            }
+        if (!count)
+                {    auto iter = find(notcoveredmins.begin(),notcoveredmins.end(),it->first);
+                        if(iter == notcoveredmins.end())
+                            notcoveredmins.push_back(it->first);
+                }
+        }
+        
+    }
+    print(EPIs, notcoveredmins, variables);
+}
+#endif
