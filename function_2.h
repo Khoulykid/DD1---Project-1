b7e1c510ce2c214facbfc9bd2ff3a88eabb48c30
Khoulykid <khoulykid@aucegypt.edu>
2023-11-03 21:28:45 +0200

    Added comments on each of Function 2 and 3 to describe what each part does exactly in the code for easier navigation for anyone that wishes to implement them.

--- function_2.h
+++ function_2.h
@@ -13,17 +13,18 @@ using namespace std;
 
 map<char, vector<int>> function_2(set<char> vars, vector<string> pros, bool PoS)
 {
-    int n = vars.size();
-    int size = 2 << n - 1;
-    map<char, vector<int>> table;
+    int n = vars.size();    //Storing it in a varialbe instead of calling the function everytime.
+    int size = 2 << n - 1;  //Bit operation to get the number of rows in the truth table.
+    map<char, vector<int>> table;   //Creating a map(truth table) with Key index as the variables in the input
+                                    //and frequency as the vector of int that'll store the values (0,1) 
 
     for (set<char>::iterator iter = vars.begin(); iter != vars.end(); iter++)
-        table.insert(pair<char, vector<int>>(*iter, vector<int>(size)));
+        table.insert(pair<char, vector<int>>(*iter, vector<int>(size)));    //Initializing the variables in the Map (Key)
 
-    table.insert(pair<char, vector<int>>('X', vector<int>(size)));
+    table.insert(pair<char, vector<int>>('X', vector<int>(size)));  //Making an X as the final column to store the answers of our function
     set<char>::iterator iter = vars.end();
     iter--;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++)    //Loop to fill the table with 1's and 0's
     {
         for (int j = 0; j < size; j++)
         {
@@ -36,42 +37,42 @@ map<char, vector<int>> function_2(set<char> vars, vector<string> pros, bool PoS)
     
 
     
-    if (!PoS)
+    if (!PoS)   //checks whether it is PoS or not
     {
         for (int j = 0; j < pros.size(); j++)
         {
 
             for (int i = 0; i < size; i++)
             {
-                if (table['X'][i] == 0)
+                if (table['X'][i] == 0) //This is so he doesn't edit a minterm that is already true
                 {
                     int final = 1;
                     for (int k = 0; k < pros[j].length(); k++)
                     {
                         if (pros[j][k+1] == '\'')
-                            final = final && !table[pros[j][k++]][i];
+                            final = final && !table[pros[j][k++]][i];   //Checks whether it is inverted or not
                         else
                             final = final && table[pros[j][k]][i];
                     }
-                    table['X'][i] = final;
+                    table['X'][i] = final;  //Makes the minterm on or off depending on the product
                 }
             }
         }
     }
     else
     {
-        replace(table['X'].begin(),table['X'].end(), 0, 1 );
+        replace(table['X'].begin(),table['X'].end(), 0, 1 );    //Replacing all the 0's with 1's so I can make the maxterms = 0
         for (int j = 0; j < pros.size(); j++)
         {
 
             for (int i = 0; i < size; i++)
             {
-                if (table['X'][i] == 1)
+                if (table['X'][i] == 1) //Check if it's 1 so that I do not edit over an already existing 0
                 {
                     int final = 1;
-                    for (int k = 0; k < pros[j].length(); k+=2)
+                    for (int k = 0; k < pros[j].length(); k+=2) //Iterate by 2 steps to avoid checking over the +
                     {
-                        if (pros[j][k+1] == '\'')
+                        if (pros[j][k+1] == '\'')   //Checking for invert
                             final = final && table[pros[j][k++]][i];
                         else
                             final = final && !table[pros[j][k]][i];
@@ -85,7 +86,7 @@ map<char, vector<int>> function_2(set<char> vars, vector<string> pros, bool PoS)
     vector<string> sums;
     vector<string> products;
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)  //Producing the products/minterms
     {
         if(table['X'][i])
         {
@@ -100,7 +101,7 @@ map<char, vector<int>> function_2(set<char> vars, vector<string> pros, bool PoS)
         }
     }
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)  //Producing the sums/Maxterms
     {
         if(!table['X'][i])
         {
